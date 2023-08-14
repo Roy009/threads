6c998abdb9a10a37c7cae2b36e6ad2ca3fa46efc
Rohit009 <63160366+Roy009@users.noreply.github.com>
2023-08-15 00:20:53 +0530

    Account Profile

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -1,7 +1,34 @@
 "use client"
-const AccountProfile = () => {
+
+import { Form } from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { UserValidation } from "@/lib/validations/user";
+interface Props {
+    user: {
+        id: string;
+        objectId: string;
+        username: string;
+        name: string;
+        bio:string;
+        image: string;
+    };
+    btnTitle: string;
+}
+const AccountProfile = ({user, btnTitle}: Props) => {
+    const form = useForm({
+        resolver: zodResolver(UserValidation),
+        defaultValues: {
+            profile_photo: '',
+            name: '',
+            username: '',
+            bio: '',
+        }
+    })
     return (
-        <div>Account profile</div>
+        <Form>
+            
+        </Form>
     )
 }
 
